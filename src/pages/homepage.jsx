d9ca417165a8f4d9ab363dc0593389bb4c70003b
onderakbulut <onderakbulut@yahoo.com>
2024-10-14 15:49:24 +0300

    Add Portfolio component and styles

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,11 +1,13 @@
 import Hero from '@/components/hero/hero'
 import Services from '@/components/services/services'
+import Portfolio from '@/components/portfolio/portfolio'
 
 function HomePage() {
     return (  
         <div>
             <Hero />
             <Services />
+            <Portfolio />
         </div>
     );
 }
